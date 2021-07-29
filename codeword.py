2dda91a21c6b8e17df2cc8bcfe5227930e42b88b
ExpertiseCyberInsights <88106617+ExpertiseCyberInsights@users.noreply.github.com>
2021-07-29 09:28:46 +0530

    Create codeword.py
    pkg upgrade && pkg update
    pkg install git 
    pkg install python
    git clone https://github.com/ExpertiseCyberInsights/codeword.git
    cd codeword
    chmod +x
    python codeword.py

---
+++ codeword.py
@@ -0,0 +1,46 @@
+#python
+import os
+os.system("clear")
+ 
+print("\033[1;32;40m CyberClan The IT society of SGNDKC\n")
+print("\033[1;33;40m This tool halping you to get new hint for your new question\n")
+ 
+print("""\033[1;32;40m                                                                               _____
+
+ $$$$$$\            $$\                            $$$$$$\  $$\                     
+$$  __$$\           $$ |                          $$  __$$\ $$ |                    
+$$ /  \__|$$\   $$\ $$$$$$$\   $$$$$$\   $$$$$$\  $$ /  \__|$$ | $$$$$$\  $$$$$$$\  
+$$ |      $$ |  $$ |$$  __$$\ $$  __$$\ $$  __$$\ $$ |      $$ | \____$$\ $$  __$$\ 
+$$ |      $$ |  $$ |$$ |  $$ |$$$$$$$$ |$$ |  \__|$$ |      $$ | $$$$$$$ |$$ |  $$ |
+$$ |  $$\ $$ |  $$ |$$ |  $$ |$$   ____|$$ |      $$ |  $$\ $$ |$$  __$$ |$$ |  $$ |
+\$$$$$$  |\$$$$$$$ |$$$$$$$  |\$$$$$$$\ $$ |      \$$$$$$  |$$ |\$$$$$$$ |$$ |  $$ |
+ \______/  \____$$ |\_______/  \_______|\__|       \______/ \__| \_______|\__|  \__|
+          $$\   $$ |                                                                
+          \$$$$$$  |                                                                
+           \______/                                                                 
+                                   ==================Created By Farhan==============""")
+ 
+\033[1;91m   ╔═══════════════════════════════════════════╗
+\033[1;93m   |                                           |
+\033[1;92m   |Author: CyberClan : The IT society of SGNDKC |
+\033[1;93m   |                                           |
+\033[1;93m   |Instagram:  https://cyberclan.sgndkc
+\033[1;93m   |                                           |
+\033[1;95m   |                          Created By Farhan 
+\033[1;93m   |                                           |
+\033[1;93m   ╚═══════════════════════════════════════════╝    
+ CorrectCodeWord = 'CyberClan.sgndkc'
+print '   \x1b[1;93m[🔒🔒🔒] CodeWord Required To Enter [🔒🔒🔒]'
+loop = 'true'
+while loop == 'true':
+    CodeWord = raw_input('\x1b[1;93m[\x1b[1;92m@Farhan@\033[1;93m] \x1b[1;91m[🔐] Enter CodeWord\x1b[1;97m: ')
+    if (CodeWord == CorrectCodeWord):
+        print('\n            \x1b[1;92m🔓🔓🔓 Correct Entry 🔓🔓🔓 \n                  ')
+          print("\033[1;32;40m The answer of your next question will be in a comments section on instagram's post username @cyber.insights01\n")
+          loop = 'false'
+    else:
+        print '\x1b[1;91mWrong Entry!'
+        loop = 'false'
+    else:
+        os.system('xdg-open https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTP5vqxD0wdg0TRi7BVH5EjfAVewnTR-Bm86g&usqp=CAU')
+
