c84bfac051342e7ac1a116d936edf90e088b37ff
ExpertiseCyberInsights <88106617+ExpertiseCyberInsights@users.noreply.github.com>
2021-07-29 10:29:30 +0530

    Update code.py

--- code.py
+++ code.py
@@ -20,18 +20,40 @@ $$ |  $$\ $$ |  $$ |$$ |  $$ |$$   ____|$$ |      $$ |  $$\ $$ |$$  __$$ |$$ |  
            \______/                                                                 
                                    ==================Created By Farhan==============""")
 
- CorrectCodeWord = 'CyberClan.sgndkc'
-print '   \x1b[1;93m[🔒🔒🔒] CodeWord Required To Enter [🔒🔒🔒]'
-loop = 'true'
-while loop == 'true':
-    CodeWord = raw_input('\x1b[1;93m[\x1b[1;92m@Farhan@\033[1;93m] \x1b[1;91m[🔐] Enter CodeWord\x1b[1;97m: ')
-    if (CodeWord == CorrectCodeWord):
-        print('\n            \x1b[1;92m🔓🔓🔓 Correct Entry 🔓🔓🔓 \n                  ')
-          print("\033[1;32;40m The answer of your next question will be in a comments section on instagram's post username @cyber.insights01\n")
-          loop = 'false'
-    else:
-        print '\x1b[1;91mWrong Entry!'
-        loop = 'false'
-    else:
-        os.system('xdg-open https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTP5vqxD0wdg0TRi7BVH5EjfAVewnTR-Bm86g&usqp=CAU')
+ username = 'user'
 
+password = 'pass'
+
+ 
+
+def restart():
+
+	ngulang = sys.executable
+
+	os.execl(ngulang, ngulang, *sys.argv)
+
+ 
+
+def main():
+
+	uname = raw_input("username : ")
+
+	if uname == CyberClan:
+
+		pwd = raw_input("password : ")
+
+		os.system('clear')
+
+		print('')
+
+ 
+
+		if pwd == password:
+
+                        print ("\033[32;1m")
+
+                        os.system("cowsay -f eyes 'Assalamu@laikum wr.wb'")
+
+                        print ("\033[35;1m")
+
+                        os.system("date")
